374ad9ad224ba5b4ff17e95d2d573ca6765ddf14
ruizterce <127963868+ruizterce@users.noreply.github.com>
2025-01-21 17:20:37 +0100

    feat(auth): revamp account page and customize Authenticator styles
    - Updated `src/app/account/page.tsx` to use the `Authenticator`
    component with customizations and integration of `useAuthValidation`.
    - Added a new component `AuthenticatedView` for handling authenticated
    views.
    - Added Redux state validation and redirection logic to the account
    page.
    - Introduced a new CSS file `src/app/account/styles.css` to override
    `Authenticator` component styles.

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,27 +1,70 @@
 "use client";
 
-import React from "react";
+import { useEffect } from "react";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { AuthEventData } from "@aws-amplify/ui";
 import "@aws-amplify/ui-react/styles.css";
-
+import "./styles.css";
+import { useRouter } from "next/navigation";
 import awsconfig from "../../../aws-exports";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { useAuthValidation } from "@/hooks/useAuthValidation";
+
 Amplify.configure(awsconfig);
 
 export default function Account() {
   return (
-    <Authenticator
-      signUpAttributes={["name"]}
-      className="mt-8 drop-shadow-lg-h shadow-none font-bold"
-    >
-      {({ signOut, user }) => {
-        return (
-          <main>
-            <h1>Hello {user?.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        );
-      }}
-    </Authenticator>
+    <div className="h-full">
+      <Authenticator
+        loginMechanisms={["email"]}
+        signUpAttributes={["name"]}
+        className="h-full flex flex-col justify-center drop-shadow-lg-h font-bold"
+      >
+        {({ signOut }) => {
+          return <AuthenticatedView signOut={signOut} />;
+        }}
+      </Authenticator>
+    </div>
   );
 }
+
+const AuthenticatedView = ({
+  signOut,
+}: {
+  signOut: ((data?: AuthEventData | undefined) => void) | undefined;
+}) => {
+  const loading = useAuthValidation();
+  const { isAuthenticated, user } = useSelector(
+    (state: RootState) => state.auth
+  );
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !isAuthenticated) {
+      router.push("/login");
+    }
+  }, [loading, isAuthenticated, router]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) return null;
+
+  return (
+    <div className="h-full pt-4 px-4 pb-6 flex flex-col items-center gap-8">
+      <h1 className="text-3xl font-bold">
+        What&apos;s up, <span className="drop-shadow-sm-h">{user?.name}</span>!
+      </h1>
+      <p>Your orders</p>
+      <button
+        className="py-1 px-4 font-bold bg-primary text-background"
+        onClick={signOut}
+      >
+        Sign out
+      </button>
+    </div>
+  );
+};
